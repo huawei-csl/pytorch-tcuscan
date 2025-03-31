e745fce5eb489dc4f3acbd95b5dc87b4b34ec781
Anastasios Zouzias 00565472 <anastasios.zouzias@huawei.com>
2025-03-31 07:50:29 +0000

    (hotfix) code hygiene on test_csr_gather

--- tests/test_csr_gather.py
+++ tests/test_csr_gather.py
@@ -11,10 +11,10 @@
 from math import ceil
 
 import pytest
-import torch
 import torch_npu  # noqa
 
 import tcuscan_ops
+import torch
 
 torch.npu.config.allow_internal_format = False
 
@@ -26,63 +26,55 @@ _CSR_GATHER_SIZES = [
     40 * 1024,
     60 * 1024,
     61 * 1024,
-    62 * 1024,
-    63 * 1024,
+    61 * 1024 - 13,
+    62 * 1024 - 123,
+    63 * 1024 - 7,
     66 * 1024,
     67 * 1024,
-    68 * 1024,
-    80 * 1024,
-    1024 * 1024,
+    68 * 1024 - 33,
+    80 * 1024 - 63,
+    1024 * 1024 - 31,
 ]
 
-
-@pytest.mark.parametrize("length", _CSR_GATHER_SIZES)
-def test_tcuscan_csr_gather_x_len_1024(length: int):
-
-    max_x_len = 1024
-
-    input_values = torch.randn(length).half().npu()
-    input_cols = torch.randint(
-        low=0, high=max_x_len, size=(length,), dtype=torch.int32
-    ).npu()
-    input_x = torch.randn(max_x_len).half().npu()
-
-    gathered_x = input_x[input_cols]
-    expected = input_values * gathered_x
-    torch.npu.synchronize()
-
-    actual = tcuscan_ops.run_csr_gather(input_values, input_cols, input_x)
-    torch.npu.synchronize()
-
-    assert actual.shape == expected.shape, "Output shape does not match expected shape."
-    assert torch.allclose(actual.float(), expected.float())
+_X_LENS = [256, 512, 1024, 2048]
 
 
 def get_profiling_lengths():
     num_cores = 20
-    s = 64
+    s = 128
     max_size = 1e8
     max_iters = ceil(max_size / (num_cores * s * s))
 
     return [i * num_cores * s * s for i in range(1, max_iters, 16 * 128 // s)]
 
 
-@pytest.mark.parametrize("length", get_profiling_lengths())
-def test_tcuscan_csr_gather_profiling_lens(length: int):
-    max_x_len = 1024
+def ref_csr_gather(input_values, input_cols, input_x):
+    return input_values * input_x[input_cols]
 
-    input_values = torch.randn(length).half().npu()
+
+def _test_tcuscan_csr_gather(col_len, x_len):
+    input_values = torch.randn(col_len).half().npu()
     input_cols = torch.randint(
-        low=0, high=max_x_len, size=(length,), dtype=torch.int32
+        low=0, high=x_len, size=(col_len,), dtype=torch.int32
     ).npu()
-    input_x = torch.randn(max_x_len).half().npu()
+    input_x = torch.randn(x_len).half().npu()
 
-    gathered_x = input_x[input_cols]
-    expected = input_values * gathered_x
+    expected = ref_csr_gather(input_values, input_cols, input_x)
     torch.npu.synchronize()
-
     actual = tcuscan_ops.run_csr_gather(input_values, input_cols, input_x)
     torch.npu.synchronize()
 
     assert actual.shape == expected.shape, "Output shape does not match expected shape."
     assert torch.allclose(actual.float(), expected.float())
+
+
+@pytest.mark.parametrize("col_len", _CSR_GATHER_SIZES)
+@pytest.mark.parametrize("x_len", _X_LENS)
+def test_tcuscan_csr_gather(col_len: int, x_len: int):
+    _test_tcuscan_csr_gather(col_len, x_len)
+
+
+@pytest.mark.parametrize("col_len", get_profiling_lengths())
+@pytest.mark.parametrize("x_len", _X_LENS)
+def test_tcuscan_csr_gather_profiling_lens(col_len: int, x_len: int):
+    _test_tcuscan_csr_gather(col_len, x_len)
