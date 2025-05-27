6ed8d0570c75e0c762328f9fc03d4b88239553c7
Anastasios Zouzias 00565472 <anastasios.zouzias@huawei.com>
2025-05-27 08:53:42 +0000

    Resolve "Code hygiene on block-scan kernel"

--- src/kernels/kernel_block_scan.h
+++ src/kernels/kernel_block_scan.h
@@ -38,11 +38,14 @@ using namespace kernel_utils;
  * 2. \f$X_2 = A U\f$;
  * 3. \f$X_2 \mathrel{+}= L X_1\f$.
  *
+ * @tparam T Input data type. Supported type is `half` / `fp16`.
  * @tparam SyncAfter If true, enables synchronization after each matrix tile
  * iteration with vector cores.
  */
-template <bool SyncAfter = false>
+template <typename T = half, bool SyncAfter = false>
 class KernelBlockScan {
+  using OutputT = kernel_utils::cube_unit::CubeOutType_t<T>;
+
  public:
   /**
    * @brief Class constructor.
@@ -82,28 +85,28 @@ class KernelBlockScan {
   __aicore__ inline void Init(GM_ADDR input, GM_ADDR upper, GM_ADDR lower,
                               GM_ADDR output) {
     global_input_.SetGlobalBuffer(
-        (__gm__ half *)input + (GetBlockIdx() * a_cube_tile_size_ * num_tiles_),
+        (__gm__ T *)input + (GetBlockIdx() * a_cube_tile_size_ * num_tiles_),
         a_cube_tile_size_ * num_tiles_);
-    global_upper_.SetGlobalBuffer((__gm__ half *)upper, b_cube_tile_size_);
-    global_lower_.SetGlobalBuffer((__gm__ half *)lower);
+    global_upper_.SetGlobalBuffer((__gm__ T *)upper, b_cube_tile_size_);
+    global_lower_.SetGlobalBuffer((__gm__ T *)lower);
     global_output_.SetGlobalBuffer(
-        (__gm__ float *)output +
+        (__gm__ OutputT *)output +
             (GetBlockIdx() * c_cube_tile_size_ * num_tiles_),
         c_cube_tile_size_ * num_tiles_);
 
-    pipe.InitBuffer(a1_q_, 1, a_cube_tile_size_ * sizeof(half));
-    pipe.InitBuffer(b1_1_q_, 1, b_cube_tile_size_ * sizeof(half));
-    pipe.InitBuffer(b1_2_q_, 1, b_cube_tile_size_ * sizeof(half));
+    pipe.InitBuffer(a1_q_, 1, a_cube_tile_size_ * sizeof(T));
+    pipe.InitBuffer(b1_1_q_, 1, b_cube_tile_size_ * sizeof(T));
+    pipe.InitBuffer(b1_2_q_, 1, b_cube_tile_size_ * sizeof(T));
 
-    pipe.InitBuffer(a2_1_q_, 1, a_cube_tile_size_ * sizeof(half));
-    pipe.InitBuffer(a2_2_q_, 1, a_cube_tile_size_ * sizeof(half));
+    pipe.InitBuffer(a2_1_q_, 1, a_cube_tile_size_ * sizeof(T));
+    pipe.InitBuffer(a2_2_q_, 1, a_cube_tile_size_ * sizeof(T));
 
-    pipe.InitBuffer(b2_1_q_, 1, b_cube_tile_size_ * sizeof(half));
-    pipe.InitBuffer(b2_2_q_, 1, b_cube_tile_size_ * sizeof(half));
+    pipe.InitBuffer(b2_1_q_, 1, b_cube_tile_size_ * sizeof(T));
+    pipe.InitBuffer(b2_2_q_, 1, b_cube_tile_size_ * sizeof(T));
 
-    pipe.InitBuffer(b1_buf_, b_cube_tile_size_ * sizeof(half));
+    pipe.InitBuffer(b1_buf_, b_cube_tile_size_ * sizeof(T));
 
-    pipe.InitBuffer(co1_q_, 1, c_cube_tile_size_ * sizeof(float));
+    pipe.InitBuffer(co1_q_, 1, c_cube_tile_size_ * sizeof(OutputT));
   }
 
   /**
@@ -114,50 +117,50 @@ class KernelBlockScan {
     kernel_utils::copy::CopyGmToL1A(a1_q_, global_lower_, m_blocks_, k_blocks_);
 
     // A2: L
-    copy::CopyL1ToL0A<half, true>(a2_2_q_, a1_q_, m_blocks_, k_blocks_);
+    copy::CopyL1ToL0A<T, true>(a2_2_q_, a1_q_, m_blocks_, k_blocks_);
 
     // A2: L, A1: L, B1: [1]
-    LocalTensor<half> all_ones_1_lt_ = b1_buf_.Get<half>();
-    cube_unit::InitConstL1<half, TPosition::B1>(
+    LocalTensor<T> all_ones_1_lt_ = b1_buf_.Get<T>();
+    cube_unit::InitConstL1<T, TPosition::B1>(
         all_ones_1_lt_, 1, static_cast<uint16_t>(b_cube_tile_size_));
 
     // A2: L; B1: U; B2: [1]
     kernel_utils::copy::CopyGmToL1B(b1_1_q_, global_upper_, k_blocks_,
                                     n_blocks_);
 
-    copy::CopyL1ToL0B<half>(b2_1_q_, all_ones_1_lt_, k_blocks_, n_blocks_);
+    copy::CopyL1ToL0B<T>(b2_1_q_, all_ones_1_lt_, k_blocks_, n_blocks_);
 
     for (uint32_t idx = 0; idx < num_tiles_; ++idx) {
       // A1: A; A2: L; B1: U; B2: [1]
       copy::CopyGmToL1A(a1_q_, global_input_[idx * a_cube_tile_size_],
                         m_blocks_, k_blocks_);
       // A2: L, A; B1: U; B2: [1]
-      copy::CopyL1ToL0A<half, true>(a2_1_q_, a1_q_, m_blocks_, k_blocks_);
+      copy::CopyL1ToL0A<T, true>(a2_1_q_, a1_q_, m_blocks_, k_blocks_);
 
       // X_1 = A x [1]
       // A2: L, A; B1: U; B2: [1]; C01: X_1
-      cube_unit::Multiply<half, false /* accumulate_c */, false /* free_a*/,
+      cube_unit::Multiply<T, false /* accumulate_c */, false /* free_a*/,
                           false /* free_b */>(a2_1_q_, b2_1_q_, co1_q_, M_, N_,
                                               K_);
 
       // A2: L, A; B1: U, X_1; B2: [1]
-      copy::CopyC01ToB1<half, float>(b1_2_q_, co1_q_, M_, N_);
+      copy::CopyC01ToB1<T, float>(b1_2_q_, co1_q_, M_, N_);
 
       // A2: L, A; B1: U, X_1; B2: [1], U
-      copy::CopyL1ToL0B<half, false>(b2_2_q_, b1_1_q_, k_blocks_, n_blocks_);
+      copy::CopyL1ToL0B<T, false>(b2_2_q_, b1_1_q_, k_blocks_, n_blocks_);
 
       // X_2 = A x U
       // A2: L; B1: U, X_1; B2: [1]; C01: X_2
-      cube_unit::Multiply<half, false /* accumulate_c */, true /* free_a*/,
+      cube_unit::Multiply<T, false /* accumulate_c */, true /* free_a*/,
                           true /* free_b */>(a2_1_q_, b2_2_q_, co1_q_, M_, N_,
                                              K_);
 
       // A2: L; B1: U; B2: [1], X_1; C01: X_2
-      copy::CopyL1ToL0B<half, true>(b2_2_q_, b1_2_q_, k_blocks_, n_blocks_);
+      copy::CopyL1ToL0B<T, true>(b2_2_q_, b1_2_q_, k_blocks_, n_blocks_);
 
       // X_2 += L x X_1
       // A2: L; B1: U; B2: [1]; C01: X_2
-      cube_unit::Multiply<half, true /* accumulate_c */, false /* free_a*/,
+      cube_unit::Multiply<T, true /* accumulate_c */, false /* free_a*/,
                           true /* free_b */>(a2_2_q_, b2_2_q_, co1_q_, M_, N_,
                                              K_);
 
@@ -170,11 +173,11 @@ class KernelBlockScan {
       }
     }
     // B1: U; B2: [1];
-    queue::FreeFromQ<half>(a2_2_q_);
+    queue::FreeFromQ<T>(a2_2_q_);
     // B1: U
-    queue::FreeFromQ<half>(b2_1_q_);
+    queue::FreeFromQ<T>(b2_1_q_);
     // All empty
-    queue::FreeFromQ<half>(b1_1_q_);
+    queue::FreeFromQ<T>(b1_1_q_);
   }
 
  private:
@@ -193,23 +196,22 @@ class KernelBlockScan {
 
   TBuf<QuePosition::B1> b1_buf_;
 
-  GlobalTensor<half> global_input_;
-  GlobalTensor<half> global_upper_;
-  GlobalTensor<half> global_lower_;
-  GlobalTensor<float> global_output_;
+  GlobalTensor<T> global_input_;
+  GlobalTensor<T> global_upper_;
+  GlobalTensor<T> global_lower_;
+  GlobalTensor<OutputT> global_output_;
 
   const uint32_t vec_len_;
   const uint32_t matmul_size_;
   const uint32_t num_tiles_;
 
-  constexpr static uint32_t CUBE_BLOCK_SIZE_ = 16;
   const uint32_t K_ = matmul_size_;
   const uint32_t N_ = matmul_size_;
   const uint32_t M_ = matmul_size_;
 
-  const uint32_t n_blocks_ = N_ / CUBE_BLOCK_SIZE_;
-  const uint32_t k_blocks_ = K_ / CUBE_BLOCK_SIZE_;
-  const uint32_t m_blocks_ = M_ / CUBE_BLOCK_SIZE_;
+  const uint32_t n_blocks_ = N_ / kernel_utils::GetFractalMN<T>();
+  const uint32_t k_blocks_ = K_ / kernel_utils::GetFractalK<T>();
+  const uint32_t m_blocks_ = M_ / kernel_utils::GetFractalMN<T>();
 
   const uint32_t a_cube_tile_size_ = M_ * K_;
   const uint32_t b_cube_tile_size_ = K_ * N_;
