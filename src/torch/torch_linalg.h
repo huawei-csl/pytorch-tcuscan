05973b418d1f9f1ad6e1163c0cf1e7557f6e7113
Gioele Gottardo 84416253 <gioele.gottardo2@h-partners.com>
2025-12-17 13:30:28 +0000

    Resolve "[Inverse] Accept any valid tensor shape"

--- src/torch/torch_linalg.h
+++ src/torch/torch_linalg.h
@@ -31,30 +31,36 @@ namespace tcuscan {
  * unit upper triangular matrices.
  * @return Returns Matrix inverse for each matrix over the `batch_dim`.
  */
-at::Tensor run_tri_inv_col_sweep(const at::Tensor &x) {
+at::Tensor run_tri_inv_col_sweep(const at::Tensor& x) {
   auto acl_stream = c10_npu::getCurrentNPUStream().stream(false);
   const at::Device device = x.options().device();
   const auto dtype = x.options().dtype();
+  if (x.dim() < 2)
+    throw std::runtime_error("Input tensor must have at least 2 dimensions.\n");
 
-  const uint32_t block_dim = static_cast<uint32_t>(x.size(0));
-  const uint32_t matrix_size = static_cast<uint32_t>(x.size(1));
+  const uint32_t matrix_size = static_cast<uint32_t>(x.size(-1));
+  if (matrix_size != x.size(-2))
+    throw std::runtime_error("Only square matrices are supported.\n");
+
   const uint32_t num_elems = static_cast<uint32_t>(x.numel());
+  const uint32_t block_dim =
+      static_cast<uint32_t>(num_elems / (matrix_size * matrix_size));
 
   const at::Tensor z = at::empty_like(x);
   const at::Tensor workspace_tensor = tcuscan::alloc_workspace(0, device);
   const TriInvColumnSweepTiling tiling{block_dim, num_elems, matrix_size};
-  uint8_t *tiling_device = tcuscan::alloc_copy_tiling(tiling);
+  uint8_t* tiling_device = tcuscan::alloc_copy_tiling(tiling);
 
   if (dtype == torch::kHalf) {
     ACLRT_LAUNCH_KERNEL(tri_inv_col_sweep_fp16)
-    (block_dim, acl_stream, const_cast<void *>(x.storage().data()),
-     const_cast<void *>(z.storage().data()),
-     const_cast<void *>(workspace_tensor.storage().data()), tiling_device);
+    (block_dim, acl_stream, const_cast<void*>(x.storage().data()),
+     const_cast<void*>(z.storage().data()),
+     const_cast<void*>(workspace_tensor.storage().data()), tiling_device);
   } else {
     ACLRT_LAUNCH_KERNEL(tri_inv_col_sweep_fp32)
-    (block_dim, acl_stream, const_cast<void *>(x.storage().data()),
-     const_cast<void *>(z.storage().data()),
-     const_cast<void *>(workspace_tensor.storage().data()), tiling_device);
+    (block_dim, acl_stream, const_cast<void*>(x.storage().data()),
+     const_cast<void*>(z.storage().data()),
+     const_cast<void*>(workspace_tensor.storage().data()), tiling_device);
   }
 
   aclrtFree(tiling_device);
@@ -69,28 +75,41 @@ at::Tensor run_tri_inv_col_sweep(const at::Tensor &x) {
  * unit upper triangular matrices.
  * @return Returns Matrix inverse for each matrix over the `batch_dim`.
  */
-at::Tensor run_tri_inv_cube_col_sweep(const at::Tensor &x) {
+at::Tensor run_tri_inv_cube_col_sweep(const at::Tensor& x) {
   auto acl_stream = c10_npu::getCurrentNPUStream().stream(false);
   const at::Device device = x.options().device();
+  const auto dtype = x.options().dtype();
+  const auto dtype_out = torch::kFloat32;
+  if (x.dim() < 2)
+    throw std::runtime_error("Input tensor must have at least 2 dimensions.\n");
 
-  const uint32_t block_dim = static_cast<uint32_t>(x.size(0));
-  const uint32_t matrix_size = static_cast<uint32_t>(x.size(1));
+  const uint32_t matrix_size = static_cast<uint32_t>(x.size(-1));
+  if (matrix_size != x.size(-2))
+    throw std::runtime_error("Only square matrices are supported.\n");
+
+  const uint32_t num_elems = static_cast<uint32_t>(x.numel());
+  const uint32_t block_dim =
+      static_cast<uint32_t>(num_elems / (matrix_size * matrix_size));
 
   const at::Tensor z =
       at::empty({block_dim, matrix_size, matrix_size},
-                at::TensorOptions().dtype(at::kFloat).device(device));
+                at::TensorOptions().dtype(dtype_out).device(device));
 
   const TriInvCubeColSweepTiling tiling{block_dim, matrix_size};
-  uint8_t *tiling_device = tcuscan::alloc_copy_tiling(tiling);
-  const size_t workspace_size =
-      workspace::get_workspace_size<uint16_t /* half */>(tiling);
-  const at::Tensor workspace_tensor =
-      tcuscan::alloc_workspace(workspace_size, device);
+  uint8_t* tiling_device = tcuscan::alloc_copy_tiling(tiling);
+  if (dtype == torch::kHalf) {
+    const size_t workspace_size =
+        workspace::get_workspace_size<uint16_t /* half */>(tiling);
+    const at::Tensor workspace_tensor =
+        tcuscan::alloc_workspace(workspace_size, device);
 
-  ACLRT_LAUNCH_KERNEL(tri_inv_cube_col_sweep_fp16)
-  (block_dim, acl_stream, const_cast<void *>(x.storage().data()),
-   const_cast<void *>(z.storage().data()),
-   const_cast<void *>(workspace_tensor.storage().data()), tiling_device);
+    ACLRT_LAUNCH_KERNEL(tri_inv_cube_col_sweep_fp16)
+    (block_dim, acl_stream, const_cast<void*>(x.storage().data()),
+     const_cast<void*>(z.storage().data()),
+     const_cast<void*>(workspace_tensor.storage().data()), tiling_device);
+  } else {
+    throw std::runtime_error("Currently only half precision is supported.\n");
+  }
 
   aclrtFree(tiling_device);
   aclrtSynchronizeStream(acl_stream);
@@ -108,30 +127,37 @@ at::Tensor run_tri_inv_cube_col_sweep(const at::Tensor &x) {
  * identity
  *
  */
-at::Tensor run_triu_inv_rec_unroll(const at::Tensor &x) {
+at::Tensor run_triu_inv_rec_unroll(const at::Tensor& x) {
   auto acl_stream = c10_npu::getCurrentNPUStream().stream(false);
   const at::Device device = x.options().device();
   const auto dtype = x.options().dtype();
   const auto dtype_out = torch::kFloat32;
-  const uint32_t block_dim = static_cast<uint32_t>(x.size(0));
-  const uint32_t matrix_size = static_cast<uint32_t>(x.size(1));
+  if (x.dim() < 2)
+    throw std::runtime_error("Input tensor must have at least 2 dimensions.\n");
+
+  const uint32_t matrix_size = static_cast<uint32_t>(x.size(-1));
+  if (matrix_size != x.size(-2))
+    throw std::runtime_error("Only square matrices are supported.\n");
+
   const uint32_t num_elems = static_cast<uint32_t>(x.numel());
+  const uint32_t block_dim =
+      static_cast<uint32_t>(num_elems / (matrix_size * matrix_size));
 
   const at::Tensor z =
       at::zeros({block_dim, matrix_size, matrix_size},
                 at::TensorOptions().dtype(dtype_out).device(device));
 
   const TriuInvRecUnrollTiling tiling{block_dim, matrix_size};
-  uint8_t *tiling_device = tcuscan::alloc_copy_tiling(tiling);
+  uint8_t* tiling_device = tcuscan::alloc_copy_tiling(tiling);
 
   if (dtype == torch::kHalf) {
     uint32_t workspace_size = 3 * num_elems * sizeof(uint16_t);
     const at::Tensor workspace_tensor =
         tcuscan::alloc_zeros_workspace(workspace_size, device);
     ACLRT_LAUNCH_KERNEL(triu_inv_rec_unroll_fp16)
-    (block_dim, acl_stream, const_cast<void *>(x.storage().data()),
-     const_cast<void *>(z.storage().data()),
-     const_cast<void *>(workspace_tensor.storage().data()), tiling_device);
+    (block_dim, acl_stream, const_cast<void*>(x.storage().data()),
+     const_cast<void*>(z.storage().data()),
+     const_cast<void*>(workspace_tensor.storage().data()), tiling_device);
   } else {
     throw std::runtime_error("Currently only half precision is supported.\n");
   }
