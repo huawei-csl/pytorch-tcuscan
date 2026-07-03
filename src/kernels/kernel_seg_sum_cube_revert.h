4035fda65d6d8531ac615949b7d6cb166d3d4864
Gioele Gottardo <72650449+gioelegott@users.noreply.github.com>
2026-07-03 15:02:40 +0200

    Remove DisableDmaAtomic Fix (#48)

--- src/kernels/kernel_seg_sum_cube_revert.h
+++ src/kernels/kernel_seg_sum_cube_revert.h
@@ -116,7 +116,6 @@ class KernelSegSumCubeRevert {
       copy::CopyVecToGm(global_out_[out_offset_], out_q_, tile_len_);
       if constexpr (UseAtomicWrite) {
         AscendC::SetAtomicNone();
-        AscendC::DisableDmaAtomic();
       }
       out_offset_ += tile_len_;
       vec_out_lt = out_q_.template AllocTensor<T>();
@@ -237,7 +236,6 @@ class KernelSegSumCubeRevert {
     copy::CopyVecToGm(global_out_[out_offset_], out_q_, tail_len);
     if constexpr (UseAtomicWrite) {
       AscendC::SetAtomicNone();
-      AscendC::DisableDmaAtomic();
     }
   }
 
