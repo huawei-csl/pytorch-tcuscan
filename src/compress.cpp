cb9dcfba56e15aac9adc1be8b91123b080a4a2e2
Anastasios Zouzias <anastasios.zouzias@huawei.com>
2026-07-26 09:31:57 +0000

    fix(compress): fix utests
    The following command will pass all tests.
    
    make clean build test_compress test_compress_ind
    test_compress_ind_no_arange

--- src/compress.cpp
+++ src/compress.cpp
@@ -27,8 +27,7 @@ extern "C" __global__ __aicore__ void compress_fp16(GM_ADDR x, GM_ADDR mask,
                                                     GM_ADDR z,
                                                     GM_ADDR workspace,
                                                     GM_ADDR tiling_gm) {
-  KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_AIV_ONLY);
-
+  KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_MIX_AIC_1_2);
 
   tcuscan::CompressTiling tiling;
   GetTilingData(&tiling, tiling_gm);
@@ -52,7 +51,7 @@ extern "C" __global__ __aicore__ void compress_fp32(GM_ADDR x, GM_ADDR mask,
                                                     GM_ADDR z,
                                                     GM_ADDR workspace,
                                                     GM_ADDR tiling_gm) {
-  KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_AIV_ONLY);
+  KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_MIX_AIC_1_2);
 
   tcuscan::CompressTiling tiling;
   GetTilingData(&tiling, tiling_gm);
@@ -136,7 +135,7 @@ extern "C" __global__ __aicore__ void compress_ind_fp32(
 extern "C" __global__ __aicore__ void compress_ind_no_arange_fp16(
     GM_ADDR vec_in, GM_ADDR mask, GM_ADDR num_ones_per_block, GM_ADDR vec_out,
     GM_ADDR indices_out, GM_ADDR workspace, GM_ADDR tiling_gm) {
-  KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_AIV_ONLY);
+  KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_MIX_AIC_1_2);
 
   (void)workspace;
   tcuscan::CompressTiling tiling;
@@ -165,7 +164,7 @@ extern "C" __global__ __aicore__ void compress_ind_no_arange_fp16(
 extern "C" __global__ __aicore__ void compress_ind_no_arange_fp32(
     GM_ADDR vec_in, GM_ADDR mask, GM_ADDR num_ones_per_block, GM_ADDR vec_out,
     GM_ADDR indices_out, GM_ADDR workspace, GM_ADDR tiling_gm) {
-  KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_AIV_ONLY);
+  KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_MIX_AIC_1_2);
 
   (void)workspace;
   tcuscan::CompressTiling tiling;
