879fac0b8561181b615aeb963c466c820181e27d
Anastasios Zouzias <zouzias@users.noreply.github.com>
2026-08-07 15:20:56 +0200

    fix(kernels): remove AscendC::GetUserWorkspace (#107)
    ---------
    
    Co-authored-by: Anastasios Zouzias <anastasios.zouzias@huawei.com>

--- src/split.cpp
+++ src/split.cpp
@@ -27,9 +27,7 @@ extern "C" __global__ __aicore__ void split_uint16(GM_ADDR in, GM_ADDR mask,
   tcuscan::SplitTiling tiling_data;
   GetTilingData(&tiling_data, tiling);
 
-  GM_ADDR const usrWorkspace = AscendC::GetUserWorkspace(workspace);
-
-  tcuscan::run_split_uint16(in, mask, out, usrWorkspace, tiling_data.num_elems,
+  tcuscan::run_split_uint16(in, mask, out, workspace, tiling_data.num_elems,
                             tiling_data.vec_tile_size, zeros_first);
 }
 
@@ -51,9 +49,7 @@ extern "C" __global__ __aicore__ void split_ind_uint16(
   tcuscan::SplitTiling tiling_data;
   GetTilingData(&tiling_data, tiling);
 
-  GM_ADDR const usrWorkspace = AscendC::GetUserWorkspace(workspace);
-
   tcuscan::run_split_ind_uint16(
-      vec_in, mask_in, indices_in, vec_out, indices_out, usrWorkspace,
+      vec_in, mask_in, indices_in, vec_out, indices_out, workspace,
       tiling_data.num_elems, tiling_data.vec_tile_size, zeros_first);
 }
